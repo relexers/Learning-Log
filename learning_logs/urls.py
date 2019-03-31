54784d135c98a863854326251c13761b024ff8d2
relexers <mr.dmitrij.kiselev@gmail.com>
2019-03-31 23:42:45 +0300

    Ready for deployment to heroku.

---
+++ learning_logs/urls.py
@@ -0,0 +1,27 @@
+"""learning_log URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from . import views
+from django.urls import path
+
+
+urlpatterns = [
+    path('', views.index, name = 'index'),
+    path(r'^topics/$', views.topics, name = 'topics'),
+    path(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+    path(r'^new_topic/$', views.new_topic, name = 'new_topic'),
+    path(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry'),
+    path(r'^edit_entry/(?P<entry_id>\d+)/$', views.edit_entry, name='edit_entry'),
+]
